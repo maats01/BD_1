60f2bca5765a6f0da056efe0d6522fbaae8d4497
Matheus <matheusp.sampaio@gmail.com>
2024-06-08 20:14:19 -0300

    atualizando seeding

--- Exercícios/Transporte Aéreo/seedingScripts/seedingScript.py
+++ Exercícios/Transporte Aéreo/seedingScripts/seedingScript.py
@@ -13,7 +13,37 @@ conn = mysql.connector.connect(
 cursor = conn.cursor()
 fake.name
 
-# inserting data in AEROPORTOS/ESCALAS
+# inserindo tipos de aeronaves
+cursor.execute("INSERT INTO tipos_aeronaves(TIPO, DESC_TIPO) VALUES('Jato Comercial', 'Aeronave de passageiros de grande porte')")
+cursor.execute("INSERT INTO tipos_aeronaves(TIPO, DESC_TIPO) VALUES('Jato Executivo', 'Aeronave de passageiros de pequeno porte')")
+cursor.execute("INSERT INTO tipos_aeronaves(TIPO, DESC_TIPO) VALUES('Jato regional', 'Aeronave de médio alcance')")
+cursor.execute("INSERT INTO tipos_aeronaves(TIPO, DESC_TIPO) VALUES('Aeronave de Carga', 'Aeronave utilizada exclusivamente para carga')")
+cursor.execute("INSERT INTO tipos_aeronaves(TIPO, DESC_TIPO) VALUES('Turboélice Comercial', 'Aeronave de passageiros com motores turboélice')")
+
+# inserindo registros em fabricantes
+cursor.execute("INSERT INTO fabricantes(FABRICANTE) VALUES('Airbus')")
+cursor.execute("INSERT INTO fabricantes(FABRICANTE) VALUES('Boeing')")
+cursor.execute("INSERT INTO fabricantes(FABRICANTE) VALUES('Gulfstream')")
+cursor.execute("INSERT INTO fabricantes(FABRICANTE) VALUES('Cessna')")
+cursor.execute("INSERT INTO fabricantes(FABRICANTE) VALUES('ATR')")
+cursor.execute("INSERT INTO fabricantes(FABRICANTE) VALUES('Bombardier')")
+cursor.execute("INSERT INTO fabricantes(FABRICANTE) VALUES('Antonov')")
+cursor.execute("INSERT INTO fabricantes(FABRICANTE) VALUES('Embraer')")
+
+# inserindo registros em aeronaves
+cursor.execute("INSERT INTO aeronaves(MODELO, ID_FABRICANTE, ID_TIPO, NUM_ASSENTOS) VALUES('A320', 1, 1, 180)")
+cursor.execute("INSERT INTO aeronaves(MODELO, ID_FABRICANTE, ID_TIPO, NUM_ASSENTOS) VALUES('A350', 1, 1, 350)")
+cursor.execute("INSERT INTO aeronaves(MODELO, ID_FABRICANTE, ID_TIPO, NUM_ASSENTOS) VALUES('737', 2, 1, 160)")
+cursor.execute("INSERT INTO aeronaves(MODELO, ID_FABRICANTE, ID_TIPO, NUM_ASSENTOS) VALUES('777', 2, 1, 396)")
+cursor.execute("INSERT INTO aeronaves(MODELO, ID_FABRICANTE, ID_TIPO, NUM_ASSENTOS) VALUES('E195', 8, 3, 120)")
+cursor.execute("INSERT INTO aeronaves(MODELO, ID_FABRICANTE, ID_TIPO, NUM_ASSENTOS) VALUES('G650', 3, 2, 18)")
+cursor.execute("INSERT INTO aeronaves(MODELO, ID_FABRICANTE, ID_TIPO, NUM_ASSENTOS) VALUES('Citation X', 4, 2, 12)")
+cursor.execute("INSERT INTO aeronaves(MODELO, ID_FABRICANTE, ID_TIPO, NUM_ASSENTOS) VALUES('72', 5, 5, 12)")
+cursor.execute("INSERT INTO aeronaves(MODELO, ID_FABRICANTE, ID_TIPO, NUM_ASSENTOS) VALUES('Dash 8', 6, 5, 12)")
+cursor.execute("INSERT INTO aeronaves(MODELO, ID_FABRICANTE, ID_TIPO, NUM_ASSENTOS) VALUES('747-8F', 2, 4, 0)")
+cursor.execute("INSERT INTO aeronaves(MODELO, ID_FABRICANTE, ID_TIPO, NUM_ASSENTOS) VALUES('An-124', 7, 4, 0)")
+
+# inserindo registros em aeroporto/escalas
 for _ in range(10):
     city = fake.city()
     state = fake.state()
@@ -21,7 +51,7 @@ for _ in range(10):
     cursor.execute(f"INSERT INTO aeroportos(CIDADE, ESTADO, PAIS) VALUES ('{city}', '{state}', '{country}')")
     cursor.execute(f"INSERT INTO escalas(ID_AEROPORTO) VALUES ({cursor.lastrowid})")
 
-# inserting data in PESSOAS/PILOTOS/PASSAGEIROS
+# inserindo registros em pessoas/pilotos/passageiros
 for i in range(600):
     name = fake.name()
     cursor.execute(f"INSERT INTO pessoas (NOME_COMPLETO) VALUES ('{name}')")
@@ -31,7 +61,7 @@ for i in range(600):
     else:
         cursor.execute(f"INSERT INTO passageiros (ID_PESSOA) VALUES ({lastPersonId})")
 
-# inserting data in ASSENTOS
+# inserindo registros em assentos
 for i in range(80):
     for j in range(3):
         pos = ["JANELA", "MEIO", "CORREDOR"]
